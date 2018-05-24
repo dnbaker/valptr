2426b6e0e450ef96dce651d956eea7ca3eb43b5e
dnbaker <dnb@cs.jhu.edu>
2018-05-24 13:45:22 -0400

    Additional utility.

--- valptr.h
+++ valptr.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <cassert>
+#include <stdexcept>
+#include <utility>
 #include <cstdint>
 #include <cstdlib>
+#include <cmath>
 
 namespace vpr {
 using std::uint64_t;
@@ -12,6 +16,16 @@ using std::int32_t;
 using std::int16_t;
 using std::int8_t;
 
+inline unsigned clz(unsigned val) {
+    return __builtin_clz(val);
+}
+inline unsigned clz(unsigned long val) {
+    return __builtin_clzl(val);
+}
+inline unsigned clz(unsigned long long val) {
+    return __builtin_clzll(val);
+}
+
 template<typename T>
 struct ConstFnHelper {
     void operator()(T *val) const {
@@ -20,7 +34,7 @@ struct ConstFnHelper {
     void operator()(const T *val) const {
         this->operator()(const_cast<T *>(val));
     }
-}
+};
 
 template<typename T>
 struct DeleteFunctor: ConstFnHelper<T> {
@@ -36,7 +50,7 @@ template<typename T>
 struct DoNothingFunctor: ConstFnHelper<T> {
     // Does nothing
     void operator()(T *val) const {}
-}
+};
 
 template<typename T>
 struct FreeFunctor: ConstFnHelper<T> {
@@ -51,32 +65,68 @@ class valptr {
     static constexpr unsigned ALN_LEFTOVERS = std::log2(alignof(T));
     static constexpr uint64_t MASK          = (static_cast<uint64_t>(1) << 48) - 1;
     static constexpr uint64_t ALN_MASK      = (static_cast<uint64_t>(1) << ALN_LEFTOVERS) - 1;
-    static constexpr uint64_t ZERO_VAL_MASK = ~(MASK << ALN_LEFTOVERS);
+    static constexpr uint64_t ZERO_VAL_MASK = ~(MASK & ~ALN_MASK);
 
     uint64_t    val_;
     DelFunctor func_;
+public:
     valptr(V val, T *ptr) {
-        val_ = reinterpret_cast<uint64_t>(ptr) << ALN_LEFTOVERS;
+        std::fprintf(stderr, "ptr: %p\n", (void *)ptr);
+        std::fprintf(stderr, "ptr string: %s\n", ((std::string *)ptr)->data());
+        val_ = reinterpret_cast<uint64_t>(ptr);
+        assert((val_ & ALN_MASK) == 0);
         setval(val);
+#if !NDEBUG
+        std::fprintf(stderr, "ptr after insertiOn: %p. MASK: %llx. LEFTOVERS: %u. ZERO_VAL_MASK: %llx\n",
+                     get(), MASK, ALN_LEFTOVERS, ZERO_VAL_MASK);
+#endif
     }
     void setval(V val) {
-        val_ &= ZERO_VAL_MASK; // Set val bits to 0.
-        val_ |= (val << (48 - ALN_LEFTOVERS)) | (val & ALN_MASK);
+#if !NDEBUG
+        std::fprintf(stderr, "val before: %llx. zvmask: %llx\n", val_, ZERO_VAL_MASK);
+#endif
+        val_ &= ~ZERO_VAL_MASK; // Set val bits to 0.
+#if 0
+        uint64_t val64 = static_cast<uint64_t>(val);
+        if(val64)
+            val_ |= (val64 << (48 - ALN_LEFTOVERS)) | (static_cast<uint64_t>(val) & ALN_MASK);
+#endif
     }
     void setvalcheck(V val) {
         if(__builtin_expect(val >= (static_cast<uint64_t>(1) << (ALN_LEFTOVERS + 16)), 0))
             throw std::runtime_error("val is too large to encode.");
         setval(val);
     }
-    const T *ptr() const {
-        return ((val_ >> ALN_LEFTOVERS) & MASK) << ALN_LEFTOVERS;
+    const T *get() const {
+#if !NDEBUG
+        const auto ptr = reinterpret_cast<const T *>((val_ & MASK) & ~ALN_MASK);
+        std::fprintf(stderr, "ptr: %p\n", (void *)ptr);
+#else
+        return reinterpret_cast<const T *>(((val_ >> ALN_LEFTOVERS) & MASK) << ALN_LEFTOVERS);
+#endif
+    }
+    T       *get()       {
+#if !NDEBUG
+        auto ptr = reinterpret_cast<T *>((val_ & MASK) & ~ALN_MASK);
+        std::fprintf(stderr, "ptr: %p\n", (void *)ptr);
+#else
+        return reinterpret_cast<T *>(((val_ >> ALN_LEFTOVERS) & MASK) << ALN_LEFTOVERS);
+#endif
     }
     V        val() const {
         return V(val_ & ALN_MASK) | V(val_ >> (48 - ALN_LEFTOVERS));
     }
     ~valptr() {
-        func(ptr());
+        func_(get());
     }
 };
+
+#if 0
+template<typename T, typename V=std::uint32_t, typename DelFunctor=DeleteFunctor<T>, typename... Args>
+valptr<T, V, DelFunctor> make_unique(V val, Args &&... args) {
+    using RetType = valptr<T, V, DelFunctor>;
+    return RetType(val, new std::forward<Args>(args)...);
+}
+#endif
 
 } // namespace vpr
